d6f37c73c42757edfb66c1c36da65ac2ce744189
Khuram Zahid <khuram.zahid@confiz.local>
2020-09-25 10:10:41 +0500

    Component 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 export default function App () {
+  const [state, setState] = useState({
+    dishes: DISHES
+  });
   return (
     <div>
       <Navbar dark color="primary">
@@ -11,7 +15,7 @@ export default function App () {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu {...state} />
     </div>
   );
 }
