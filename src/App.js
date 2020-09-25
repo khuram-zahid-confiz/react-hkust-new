955492bba3857f1acefb1fcd9130d37d9cab94d9
Khuram Zahid <khuram.zahid@confiz.local>
2020-09-25 16:14:11 +0500

    Router 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Menu from './components/MenuComponent';
 import Header from './components/HeaderComponent';
 import Footer from './components/FooterComponent';
+import Home from './components/HomeComponent';
 import { DISHES } from './shared/dishes';
 
 export default function App () {
@@ -10,10 +12,16 @@ export default function App () {
     dishes: DISHES
   });
   return (
-    <div>
-      <Header />
-      <Menu {...state} />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+          <Switch>
+            <Route path='/home' component={ () => <Home /> } />
+            <Route exact path='/menu' component={ () => <Menu {...state} /> } />
+            <Redirect to="/home" />
+          </Switch>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
