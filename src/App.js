7f85fbe593fe097911b331a65b6d702561367291
Khuram Zahid <khuram.zahid@confiz.local>
2020-09-28 10:19:10 +0500

    SPA Part 1

--- src/App.js
+++ src/App.js
@@ -5,19 +5,31 @@ import Menu from './components/MenuComponent';
 import Header from './components/HeaderComponent';
 import Footer from './components/FooterComponent';
 import Home from './components/HomeComponent';
+import Contact from './components/ContactComponent';
 import { DISHES } from './shared/dishes';
+import { COMMENTS } from './shared/comments';
+import { PROMOTIONS } from './shared/promotions';
+import { LEADERS } from './shared/leaders';
 
 export default function App () {
-  const [state, setState] = useState({
-    dishes: DISHES
-  });
+  const [dishes, setDishes] = useState(DISHES);
+  const [comments, setComments] = useState(COMMENTS);
+  const [promotions, setPromotions] = useState(PROMOTIONS);
+  const [leaders, setLeaders] = useState(LEADERS);
+
   return (
     <BrowserRouter>
       <div>
         <Header />
           <Switch>
-            <Route path='/home' component={ () => <Home /> } />
-            <Route exact path='/menu' component={ () => <Menu {...state} /> } />
+            <Route path='/home' component={ () => <Home 
+                  dish={dishes.filter((dish) => dish.featured)[0]} 
+                  promotion={promotions.filter((promo) => promo.featured)[0]} 
+                  leader={leaders.filter((leader) => leader.featured)[0]} 
+                /> 
+            } />
+            <Route exact path='/menu' component={ () => <Menu dishes={dishes} /> } />
+            <Route exact path='/contactus' component={Contact} />
             <Redirect to="/home" />
           </Switch>
         <Footer />
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,20 +1,12 @@
 import React, { useState } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-import DishdetailComponent from './DishdetailComponent';
 
 export default function Menu (props) {
 
-    const [selectedDish, setSelectedDish] = useState(null);
-
-    const onDishSelect = (dish) => {
-        setSelectedDish(dish);
-    }
-
     const menu = props.dishes.map((dish) => {
         return (
-          <div  className="col-12 col-md-5 m-1">
-            <Card key={dish.id}
-              onClick={() => onDishSelect(dish)}>
+          <div key={dish.id} className="col-12 col-md-5 m-1">
+            <Card>
               <CardImg width="100%" src={dish.image} alt={dish.name} />
               <CardImgOverlay>
                   <CardTitle>{dish.name}</CardTitle>
@@ -29,7 +21,6 @@ export default function Menu (props) {
             <div className="row">
                 {menu}
             </div>
-            <DishdetailComponent selectedDish={selectedDish} />
         </div>
     );
 }
