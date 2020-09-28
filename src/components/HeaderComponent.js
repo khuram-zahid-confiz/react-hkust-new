c57166eec8ed6a2e285d6dcf4c9d11b3bb3d8815
Khuram Zahid <khuram.zahid@confiz.local>
2020-09-28 13:56:56 +0500

    Uncontrolled Form useRef

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import React, { useRef, useState } from 'react';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+    Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 export default function Header () {
     const [isNavOpen, toggleNav] = useState(false);
+    const [isModalOpen, toggleModal] = useState(false);
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+    const rememberRef = useRef(false);
+    const handleLogin = (event) => {
+        toggleModal(!isModalOpen);
+        alert("Username: " + usernameRef.current + " Password: " + passwordRef.current
+            + " Remember: " + rememberRef.current);
+        event.preventDefault();
+
+    }
     return(
         <>
             <Navbar dark expand="md">
@@ -12,18 +25,23 @@ export default function Header () {
                     <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
                     <Collapse isOpen={ isNavOpen } navbar>
                         <Nav navbar>
-                        <NavItem>
-                            <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> About Us</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link"  to='/menu'><span className="fa fa-list fa-lg"></span> Menu</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
-                        </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> About Us</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link"  to='/menu'><span className="fa fa-list fa-lg"></span> Menu</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
+                            </NavItem>
+                        </Nav>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <Button outline onClick={()=>toggleModal(!isModalOpen)}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                            </NavItem>
                         </Nav>
                     </Collapse>
                 </div>
@@ -38,6 +56,28 @@ export default function Header () {
                     </div>
                 </div>
             </Jumbotron>
+            <Modal isOpen={isModalOpen} toggle={() => toggleModal(!isModalOpen)}>
+                <ModalHeader toggle={() => toggleModal(!isModalOpen)}>Login</ModalHeader>
+                <ModalBody>
+                    <Form onSubmit={(event) => handleLogin(event)}>
+                        <FormGroup>
+                            <Label htmlFor="username">Username</Label>
+                            <Input type="text" id="username" name="username" ref={usernameRef} onChange={(event) => usernameRef.current=event.target.value} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor="password">Password</Label>
+                            <Input type="password" id="password" name="password" ref={passwordRef} onChange={(event) => passwordRef.current=event.target.value} />
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type="checkbox" name="remember" ref={rememberRef} onChange={(event) => rememberRef.current=event.target.checked} />
+                                Remember me
+                            </Label>
+                        </FormGroup>
+                        <Button type="submit" value="submit" color="primary">Login</Button>
+                    </Form>
+                </ModalBody>
+            </Modal>
         </>
     );
 }
